2f37cd9e08b371480b54ca61e2ba94320da9029c
Ho Yeon <25444008+HoYeonJ@users.noreply.github.com>
2022-10-30 23:25:23 -0400

    finished

--- Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
+++ Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ChangeTextValue : MonoBehaviour
 {
+    [SerializeField] GameObject gameObject;
     // DIRECTIONS: The goal of this script is to show the Y coordinate of the cube on
     // the screen. You will need to add some stuff to this script to make it work...
 
@@ -13,12 +14,12 @@ public class ChangeTextValue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textMeshProUGUI.text = gameObject.transform.position.y.ToString();
     }
 }
